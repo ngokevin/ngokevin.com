5ce07f7427349a5a8d89ff0de786535fc5d80bd9
Kevin Ngo <kevin.ngo@oregonstate.edu>
2011-11-19 05:13:25 -0800

    album.js displays all images in the album with nothing fancy

--- media/javascripts/album.js
+++ media/javascripts/album.js
@@ -8,8 +8,25 @@ function makeHttpObject() {
 
   throw new Error("Could not create HTTP request object.");
 }
-document.write($('#album-slug').text());
+
+// retrieve slug to determine what directory album is in
+var slug = $('#album-slug').text();
+var album_url = "/images/gallery/" + slug + "/";
+
+// make request to index of album directory
 var request = makeHttpObject();
-//request.open("GET", "files/fruit.txt", false);
-//request.send(null);
-//print(request.responseText);
+request.open("GET", album_url , false);
+request.send(null);
+var html = request.responseText;
+
+// parse out image names and push complete url to array
+var image_regex = /href="(.*.(jpg|png))"/gi;
+var images = new Array();
+while (match = image_regex.exec(html)) {
+    images.push("<img src=" + album_url + match[1] + "/>");
+}
+
+for (image in images) {
+    document.write(images[image]);
+}
+
