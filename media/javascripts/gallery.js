78c8b5beac65632869e0ada893f96f6695ab4cc5
Kevin Ngo <kevin.ngo@oregonstate.edu>
2011-11-19 19:21:42 -0800

    tryign to use node appending to get previews up

--- media/javascripts/gallery.js
+++ media/javascripts/gallery.js
@@ -3,18 +3,19 @@
 
 // retrieve slugs to determine what directory albums are in
 var album_slugs = document.getElementsByClass("album-slug");
+var album_titles = document.getElementsByClass("album-title");
 
 // create array of relative paths to albums
-var album_urls = new Array();
+var album_dirs = new Array();
 for (var index in album_slugs) {
-    album_urls.push("/images/gallery/" + album_slugs[index].innerHTML + "/");
+    album_dirs.push("/images/gallery/" + album_slugs[index].innerHTML + "/");
 }
 
 // make request to index of album directories and add html to array
 var album_htmls = new Array();
-for (var index in album_urls) {
+for (var index in album_dirs) {
     var request = makeHttpObject();
-    request.open("GET", album_urls[index], false);
+    request.open("GET", album_dirs[index], false);
     request.send(null);
     album_htmls.push(request.responseText);
 }
@@ -25,10 +26,28 @@ var image_regex = /href="(.*.(jpg|png))"/gi;
 for (var index in album_htmls) {
     var images = new Array();
     while (match = image_regex.exec(album_htmls[index])) {
-        images.push("<img src=" + album_urls[index] + match[1] + "/>");
+        var a = document.createElement("a");
+        a.href = album_slugs[index];
+
+        var img = document.createElement("img");
+        img.src = album_dirs[index] + match[1];
+
+        a.appendChild(img);
+        images.push(a);
     }
     image_preview_arrays.push(images);
 }
 
+document.appendChild(image_preview_arrays[0]);
+var gallery = document.getElementsById("gallery");
+for (var index in image_preview_arrays) {
 
+    // create a div for the album to separate it
+    var div = document.createElement("div");
+    div.id = "album-preview";
+    div.className = "span4";
+    gallery.appendChild(div);
+
+    document.write(album_titles[index].innerHTML);
+}
 
