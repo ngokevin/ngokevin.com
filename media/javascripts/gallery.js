a819cd3b9c7725ef8ef07c61048fa942ebf1c4b5
Kevin Ngo <kevin.ngo@oregonstate.edu>
2011-11-25 12:52:07 -0800

    moved everything to functions, put all constants at top

--- media/javascripts/gallery.js
+++ media/javascripts/gallery.js
@@ -2,6 +2,8 @@
 // wok's markdown files, grabs album previews
 
 var NUM_PREVIEW_IMGS = 3;
+var THUMBNAIL_SIZE = 210;
+var THUMBNAIL_PREFIX = 'THUMB_'
 
 
 // function: getAlbum
@@ -49,29 +51,113 @@ var getAlbums = function() {
 };
 
 
+// function: loadAlbums
+// given albums (contain lists of album info), add list containing img objects
+var loadAlbums = function(albums) {
+
+    albums['images'] = new Array();
+    var image_regex = new RegExp('href="(' + THUMBNAIL_PREFIX + '.*.(jpg|png|JPG))"', 'gi');
+
+    // create list of a/img objects for each album and push to albums
+    for (var index in albums['htmls']) {
+        var images = new Array();
+
+        while (match = image_regex.exec(albums['htmls'][index])) {
+
+            // only load a certain amount of images
+            if (images.length > NUM_PREVIEW_IMGS) {
+                break;
+            }
+
+            var a = document.createElement("a");
+            a.href = albums['slugs'][index].innerHTML;
+
+            var img = new Image();
+            img.style.visibility= "hidden"; // don't display until shifted
+            img.onload = imageShift();
+            img.src = albums['dirs'][index] + match[1];
+
+            a.appendChild(img);
+            images.push(a);
+        }
+        albums['images'].push(images);
+    }
+
+    // add mouseover event handlers after albums have been loaded
+    for (var album_index in albums['images']) {
+
+        var images = albums['images'][album_index];
+        for (var image_index in images){
+
+            var img = images[image_index].firstChild;
+
+            // assign handler
+            img.orig_src = img.src;
+            img.onmouseover = imageChange(image_index, images, img);
+        }
+    }
+}
+
+
+// function: insertAlbums
+// insert albums to dom into gallery div
+var insertAlbums = function() {
+
+    var gallery = document.getElementById("gallery");
+
+    for (var index in albums['images']) {
+
+        // make a new row every four albums
+        if (index % 4 == 0) {
+            var row = document.createElement("div");
+            row.className = "row";
+            gallery.appendChild(row);
+        }
+
+        // create a div for the album to separate it
+        var div = document.createElement("div");
+        div.id = "album-preview" + index;
+        div.className = "span4";
+        div.appendChild(albums['images'][index][0]);
+
+        // create overlay text with album title
+        h3 = document.createElement("h3");
+        span = document.createElement("span");
+        span.appendChild(document.createTextNode(albums['titles'][index].innerHTML));
+        h3.appendChild(span);
+        div.appendChild(h3);
+
+        // append to row
+        row.appendChild(div);
+    }
+}
+
+
 // function: imageShift
 // onload event handler that shifts image viewport towards center
 var imageShift = function() {
 
-    var THUMBNAIL_SIZE = 210;
-    var img_box = this.getBoundingClientRect();
+    // closure holds thumbnail_size constant
+    return shift = function() {
+        var img_box = this.getBoundingClientRect();
 
-    // because we're swapping in-place, need to reset the style
-    this.style.left = "0"
-    this.style.top= "0"
+        // because we're swapping in-place, need to reset the style
+        this.style.left = "0"
+        this.style.top= "0"
 
-    // shift by closing in image towards center
-    var shift_left = (img_box.width - THUMBNAIL_SIZE) / 2;
-    if (shift_left > 0) {
-        this.style.left = "-" + shift_left + "px";
+        // shift by closing in image towards center
+        var shift_left = (img_box.width - THUMBNAIL_SIZE) / 2;
+        if (shift_left > 0) {
+            this.style.left = "-" + shift_left + "px";
+        }
+        var shift_top = (img_box.height - THUMBNAIL_SIZE) / 2;
+        if (shift_top > 0) {
+            this.style.top = "-" + shift_top + "px";
+        }
+
+        // show image after shifting
+        this.style.visibility= "visible";
     }
-    var shift_top = (img_box.height - THUMBNAIL_SIZE) / 2;
-    if (shift_top > 0) {
-        this.style.top = "-" + shift_top + "px";
-    }
-
-    // show image after shifting
-    this.style.visibility= "visible";
 
 }
 
@@ -150,73 +236,5 @@ var imageChange = function(img_index, thumbnail_array, img) {
 }
 
 var albums = getAlbums();
-
-// CREATE ARRAY OF ARRAY IMAGE OBJECTS
-var image_preview_arrays = new Array();
-var image_regex = /href="(THUMB_.*.(jpg|png|JPG))"/gi;
-for (var index in albums['htmls']) {
-    var images = new Array();
-    while (match = image_regex.exec(albums['htmls'][index])) {
-
-        // only load a certain amount of images
-        if (images.length > NUM_PREVIEW_IMGS) {
-            break;
-        }
-
-        // link to actual album
-        var a = document.createElement("a");
-        a.href = albums['slugs'][index].innerHTML;
-
-        var img = new Image();
-        img.style.visibility= "hidden"; // don't display until shifted
-        img.onload = imageShift;
-        img.src = albums['dirs'][index] + match[1];
-
-        // wrap the image in an a
-        a.appendChild(img);
-        images.push(a);
-    }
-    image_preview_arrays.push(images);
-}
-
-// ADD MOUSEOVER EVENT HANDLER
-for (var album_index in image_preview_arrays) {
-
-    var thumbnails = image_preview_arrays[album_index];
-    for (var image_index in thumbnails){
-
-        var img = thumbnails[image_index].firstChild;
-
-        // assign handler
-        img.orig_src = img.src;
-        img.onmouseover = imageChange(image_index, thumbnails, img);
-    }
-}
-
-// WRITE IMAGES TO DOM
-var gallery = document.getElementById("gallery");
-for (var index in image_preview_arrays) {
-
-    // make a new row every four albums
-    if (index % 4 == 0) {
-        var row = document.createElement("div");
-        row.className = "row";
-        gallery.appendChild(row);
-    }
-
-    // create a div for the album to separate it
-    var div = document.createElement("div");
-    div.id = "album-preview" + index;
-    div.className = "span4";
-    div.appendChild(image_preview_arrays[index][0]);
-
-    // create overlay text with album title
-    h3 = document.createElement("h3");
-    span = document.createElement("span");
-    span.appendChild(document.createTextNode(albums['titles'][index].innerHTML));
-    h3.appendChild(span);
-    div.appendChild(h3);
-
-    // append to row
-    row.appendChild(div);
-}
+loadAlbums(albums);
+insertAlbums(albums);
