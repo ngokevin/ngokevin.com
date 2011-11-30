78c6aad4c15bab677f95ace93fa205ffbb8b1232
Kevin Ngo <kevin.ngo@oregonstate.edu>
2011-11-30 04:17:05 -0800

    figured out how to grab site variables i want and change them

--- hooks/gallery.py
+++ hooks/gallery.py
@@ -3,6 +3,8 @@ def get_album_dirs(page, site):
     Adds list of album_dirs to template variables
     List needs to be in same order as list of album slugs and titles
     """
-    for album in sorted(site.categories['gallery'], key=lambda album:album['datetime']):
-        print album
+
+    if page.path == 'content/gallery.mkd':
+        for album in sorted(site['site']['categories']['gallery'], key=lambda album:album['datetime']):
+            print album
 
