c8e06935a2dcd1ecec29ee6dad7452cd3b18464f
Kevin Ngo <kevin.ngo@oregonstate.edu>
2012-03-14 00:43:25 -0700

    split thumb srcs and sizes out to own list from srcs and sizes lists in gallery hook

--- hooks/album.py
+++ hooks/album.py
@@ -4,14 +4,16 @@ import simplejson
 
 import Image
 
-GALLERY_DIR = os.path.abspath("./media/images/gallery/") + '/'
-REL_GALLERY_DIR = "/images/gallery/"
-FILE_TYPES = ["jpg", "JPG", "jpeg", "JPEG", "png", "PNG"]
+GALLERY_DIR = os.path.abspath('./media/images/gallery/') + '/'
+REL_GALLERY_DIR = '/images/gallery/'
+FILE_TYPES = ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG']
+THUMB_PREFIX = 'THUMB_'
 
 def get_image_srcs(page, templ_vars):
     """
     Wok page.template.pre hook
     Get all images in the album as relative paths
+    Binds srcs and thumb_srcs to template
     """
     if 'type' in page.meta and page.meta['type'] == 'album':
         album = page.meta
@@ -27,15 +29,25 @@ def get_image_srcs(page, templ_vars):
                 img_rel_path = REL_GALLERY_DIR + album['slug'] + '/' + img.split('/')[-1]
                 srcs.append(img_rel_path)
 
-        templ_vars['site']['srcs'] = simplejson.dumps(sorted(srcs))
+        # split full srcs and thumb srcs from srcs into two lists
+        full_srcs = []
+        thumb_srcs = []
+        for src in srcs:
+            if src.split('/')[-1].startswith(THUMB_PREFIX):
+                thumb_srcs.append(src)
+            else:
+                full_srcs.append(src)
+
+        # bind to template via json
+        templ_vars['site']['srcs'] = simplejson.dumps(sorted(full_srcs))
+        templ_vars['site']['thumb_srcs'] = simplejson.dumps(sorted(thumb_srcs))
 
 
 def get_image_sizes(page, templ_vars):
     """
     Wok page.template.pre hook
     Get all images sizes (width/height) since JS doesn't know until loaded
-    Each image size tuple corresponds to an image from get_image_srcs
-    Will be in the same order
+    Binds sizes and thumb_sizes to template
     """
     if 'type' in page.meta and page.meta['type'] == 'album':
         album = page.meta
@@ -47,13 +59,20 @@ def get_image_sizes(page, templ_vars):
             image_list = glob.glob(GALLERY_DIR + album['slug'] + '/*.' + file_type)
             srcs += image_list
 
-        srcs = sorted(srcs)
-
-        sizes = []
+        # split full srcs and thumb srcs from srcs into two lists
+        full_sizes = []
+        thumb_sizes = []
         for src in srcs:
             image = Image.open(src)
             width = image.size[0]
             height = image.size[1]
-            sizes.append([width, height])
+            size = [width, height]
 
-        templ_vars['site']['sizes'] = simplejson.dumps(sizes)
+            if src.split('/')[-1].startswith(THUMB_PREFIX):
+                thumb_sizes.append(size)
+            else:
+                full_sizes.append(size)
+
+        # bind to template via json
+        templ_vars['site']['sizes'] = simplejson.dumps(full_sizes)
+        templ_vars['site']['thumb_sizes'] = simplejson.dumps(sorted(thumb_sizes))
