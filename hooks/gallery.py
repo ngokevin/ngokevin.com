4bf68e62720a38a196bfbac30253895d35ceed5d
Kevin Ngo <ktngo09@gmail.com>
2012-05-04 02:42:27 -0700

    hackathon/boardwalk blog post

--- hooks/gallery.py
+++ hooks/gallery.py
@@ -16,23 +16,27 @@ def get_albums(page, templ_vars):
 
         # for each album
         albums = {}
-        for album in sorted(templ_vars['site']['categories']['gallery'], key=lambda album:album['datetime']):
 
-            # get absolute paths of images in album
-            images = []
-            for image_list in [glob.glob(GALLERY_DIR + album['slug'] + '/*.' + file_type) for file_type in FILE_TYPES]:
+        try:
+            for album in sorted(templ_vars['site']['categories']['gallery'], key=lambda album:album['datetime']):
 
-                # grab only several
-                if len(images) > PREVIEW_IMGS_NUM:
-                    break
+                # get absolute paths of images in album
+                images = []
+                for image_list in [glob.glob(GALLERY_DIR + album['slug'] + '/*.' + file_type) for file_type in FILE_TYPES]:
 
-                # grab only thumbnails, convert paths from absolute to relative
-                images += [REL_GALLERY_DIR + album['slug'] + '/' + image.split('/')[-1] for image in image_list if image.split('/')[-1].startswith(THUMB_PREFIX)]
+                    # grab only several
+                    if len(images) > PREVIEW_IMGS_NUM:
+                        break
 
-            # dictionary of albums
-            albums[album['slug']] = images[0:3]
+                    # grab only thumbnails, convert paths from absolute to relative
+                    images += [REL_GALLERY_DIR + album['slug'] + '/' + image.split('/')[-1] for image in image_list if image.split('/')[-1].startswith(THUMB_PREFIX)]
 
-        # add albums to template variables
-        templ_vars['site']['albums'] = albums
+                # dictionary of albums
+                albums[album['slug']] = images[0:3]
+
+            # add albums to template variables
+            templ_vars['site']['albums'] = albums
+        except KeyError:
+            pass
 
 
