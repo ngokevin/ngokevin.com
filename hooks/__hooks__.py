6fdddff92ee07162ad584363463988189cd8c6d8
Kevin Ngo <ngoke@mozilla.com>
2013-04-13 19:19:11 -0700

    add sass hook

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -1,9 +1,12 @@
+from hooks import compile_sass
+
 import thumbnail
 import gallery
 import album
 
+
 hooks = {
     'site.start': [thumbnail.create_thumbnails],
+    'site.output.post':[compile_sass],
     'page.template.pre': [gallery.get_albums, album.get_image_srcs, album.get_image_sizes]
 }
-
