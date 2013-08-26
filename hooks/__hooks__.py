d2e304c0adeafa8507520ea23219427eb077016f
Kevin Ngo <ktngo09@gmail.com>
2013-08-26 10:01:48 +0000

    disable gallery

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -6,7 +6,7 @@ import album
 
 
 hooks = {
-    'site.start': [thumbnail.create_thumbnails],
-    'site.output.post':[compile_sass],
-    'page.template.pre': [gallery.get_albums, album.get_image_srcs, album.get_image_sizes]
+    #'site.start': [thumbnail.create_thumbnails],
+    #'site.output.post':[compile_sass],
+    #'page.template.pre': [gallery.get_albums, album.get_image_srcs, album.get_image_sizes]
 }
